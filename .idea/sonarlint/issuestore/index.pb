6f9499a2c39c6ab09f747b89e1d7c8d00e8c2ec5
tiaozao <tiaozao@meizhuahuyu.com>
2020-03-29 00:04:29 +0800

    @tiaozao jdk source learn

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+D
+src/test/MyTest.java,2/9/2902e6372dd3f0fe3932b30091ef66ee81bd5b89
---
+++ src/test/MyTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.HashMap;
+
+/**
+ * 描述:
+ *
+ * <p>
+ *
+ * @author tiaozao
+ * @date 2020-03-27 10:09
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(1,1);
+        map.put(2,2);
+        map.put(3,3);
+    }
+}
